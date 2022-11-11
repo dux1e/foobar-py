7e72ddcbcbe3a66a18786a1a36bf0ba68648e233
dux1e <joak0451@gmail.com>
2022-11-11 15:34:46 +0100

    kan ikke finde ud af det

--- bomb-baby/solution.py
+++ bomb-baby/solution.py
@@ -10,8 +10,13 @@
 
 # NOTE: Man starter med 1 mach bombe og 1 fach bombe
 
+# TODO: finde ud af hvor mange layers jeg skal generate ca
+# TODO: make check in layer creation: 
+# check if the x,y or y,x exists in layer; = is in layer currently created
+# if x,y or y,x is less than all in next layer; = is not possible
 def solution(x, y):
-    root = [(1,1)]
+    root = [[(1,1)]]
+    curr_l = sum(root[0][0])
     int_x = int(x)
     int_y = int(y)
     depth = int_x
@@ -19,34 +24,49 @@ def solution(x, y):
         depth = int_y
     for each in range(depth):
         root.append(create_layer(root))
-    in_tree = check_in_tree(root, int_x, int_y)
-    if not in_tree:
-        return "impossible"
-    else:
-        return in_tree
+        root.remove(root[0])
+        # print(root)
+        if (int_x,int_y) in root[-1] or (int_y,int_x) in root[-1]:
+            return(str(each+1))
+        # for e in root[-1]:
+        #     if sum(e) > curr_l:
+        #         curr_l = sum(e)
+        # if curr_l > int_x + int_y:
+    return "impossible"
+            # if e > (int_x,int_y) < e or (int_y,int_x) < e:
+            #     return "impossible"
+    # in_tree = check_in_tree(root, int_x, int_y)
+    # if not in_tree:
+    #     return "impossible"
+    # else:
+    #     return str(in_tree)
 
 def check_in_tree(root, x,y):
     check = (x,y)
+    check_flipped = (y,x)
     i = 0
     for layer in root:
         i-=-1
-        if check in layer:
+        if check in layer or check_flipped in layer:
             return i
     return False
 
 def create_layer(root):
     # 1 mach cycle
-    # fach cycle
+    # 1 facu cycle
     layer = root[-1]
     len_layer = len(layer)
     temp = []
+    # s = 0
     for i in range(len_layer):
         # facula bomb creation - Mach cycle
-        print(layer)
         temp.append(M_cycle(layer[i]))
         # mach bomb creation Facule cycle
-        print(layer)
         temp.append(F_cycle(layer[i]))
+    # for each in temp:
+    #     s = s + sum(each)
+    # print(s)
+    # print(layer)
     return temp
 
 def M_cycle(b_in):
@@ -78,3 +98,7 @@ if __name__ == '__main__':
     print(solution('2', '1')) # 1
     # 0 M=1 F=1
     # 1 2 1 F_cycle creates 1 M 
+    print(solution('7','19'))
+    print(solution('6','9'))
+    print(solution('90','100'))
+    # print(solution('98431','8730912'))
